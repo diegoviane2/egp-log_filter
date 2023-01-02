0258a1b35f0264eb334e1ac4351ff3ff3ea8cc8a
Diego <digaodigao1@gmail.com>
2023-01-02 00:03:22 -0300

    studying new filters

--- main.py
+++ main.py
@@ -10,17 +10,33 @@ import sys
 import re
 import csv
 
-dirname = 'log_files'
+dirname = 'log_files/*.log'
 
-ext = '.log'
+years = ['2020','2021','2022','2023']
+
+months = ['1','2','3','4','5','6','7','8','9','10','11','12']
+
+test = 2022
 
 # for file in os.listdir(dirname):
 #     if file.endswith(ext):
 #         with open(file):
+
+cnt = 0
+
+def filter_year(c_year,row):
+    global yearly
+    global cnt
+    date = datetime.datetime.strptime(str(row[0]), '%d/%m/%y')
+    # date = datetime.datetime.strftime(date, '%d/%m/%y')
+    if date.year == c_year:
+        yearly.append(row)
+        cnt = cnt + 1
             
-files= glob.glob("log_files/*.log")
+files= glob.glob(dirname)
 
 data = []
+yearly = []
 
 count = 0
 
@@ -33,17 +49,19 @@ for filename in files:
             
             if len(line) > 5:
                 if len(line[0]) == 10:
-                    print('IN')
+                    # print('IN')
                     count = count +1
                     date = datetime.datetime.strptime(str(line[0]), '%d/%m/%Y')
                     line[0] = datetime.datetime.strftime(date, '%d/%m/%y')
                     row = [line[0], line[1]]
                     data.append(row)
+                    filter_year(test,row)
                 else:
-                    print('OUT')
+                    # print('OUT')
                     count = count +1
                     row = [line[0], line[1]]
                     data.append(row)
+                    filter_year(test,row)
                 
                 
                 
@@ -52,8 +70,11 @@ with open('data.csv', 'w', encoding='UTF8') as f:
     writer.writerows(data)
     # print(data)                  
 
-
+print(yearly)
 print(count)
+print(cnt)
+
+
 
             # for word in re.split(r'\s+', line):
             #     print(line)
